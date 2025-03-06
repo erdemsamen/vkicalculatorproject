1fcd60aa9373fe903b1805a481f6238cd14652b4
Erdem Samen <samenerdem@gmail.com>
2025-03-06 12:34:34 +0300

    Initial commit

---
+++ src/main/java/vkicalculator.java
@@ -0,0 +1,26 @@
+
+
+import java.util.Scanner;
+
+public class vkicalculator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lütfen boyunuzu (metre cinsinden) giriniz: ");
+        double boy = scanner.nextDouble();
+        // Boyunu gösterir
+
+        System.out.println("Lütfen kilonuzu giriniz: ");
+        double kilo = scanner.nextDouble();
+        // Kilosunu gösterir
+
+        double vki = kilo / (boy * boy);
+        // Vücut kitle indeksini gösterir
+
+        System.out.println("Vücut Kitle İndeksiniz: " + vki);
+        scanner.close();
+
+
+    }
+}
