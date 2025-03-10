530effcb17942344f2aa85b90ed19dc796dd5afe
Erdem Samen <samenerdem@gmail.com>
2025-03-10 20:48:30 +0300

    Initial commit

--- src/main/java/project/VkiCalculator.java
+++ src/main/java/project/VkiCalculator.java
@@ -1,8 +1,8 @@
-package hafta1;
+package project;
 
 import java.util.Scanner;
 
-public class vkicalculator {
+public class VkiCalculator {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
